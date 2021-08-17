1ed8c33aa1935de856206d98ac08286b642c966d
bert@catsburg.com <bert@catsburg.com>
2021-08-17 18:11:20 +0200

    .

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -7,9 +7,9 @@ const PostList = () => {
 
     const [posts, setPosts] = useState({});
 
-
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4002/posts');
+        console.log(res.data);
         setPosts(res.data);
     }
 
@@ -24,7 +24,7 @@ const PostList = () => {
                     <h3>
                         {post.title}
                     </h3>
-                    <CommentList postId={post.id} />
+                    <CommentList comments={post.comments} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
